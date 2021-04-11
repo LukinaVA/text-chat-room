e0bb9069d8ebda335ba81d40fb2d7a2c6130df55
LukinaVA <lukina.va@edu.spbstu.ru>
2021-04-12 02:42:23 +0300

    Removed peerjs

--- server/server.js
+++ server/server.js
@@ -1,12 +1,7 @@
 const app = require('./app');
 const config = require('./config/config');
-const {ExpressPeerServer} = require('peer');
 const server = require('http').createServer(app);
 
-const peerServer = ExpressPeerServer(server, {
-    debug: true,
-});
-app.use('/peerjs', peerServer);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*'
