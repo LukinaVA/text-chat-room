b78a8ab7995ae21145771370217713949a4faaa2
LukinaVA <lukina.va@edu.spbstu.ru>
2021-04-12 02:24:35 +0300

    Implemented handlers

--- server/server.js
+++ server/server.js
@@ -1,60 +1,30 @@
 const app = require('./app');
 const config = require('./config/config');
-const Room = require('./models/Room');
-const {User} = require('./models/User');
-
+const {ExpressPeerServer} = require('peer');
 const server = require('http').createServer(app);
+
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+app.use('/peerjs', peerServer);
 const io = require('socket.io')(server, {
     cors: {
         origin: '*'
     }
 });
 
-io.on('connection', (socket) => {
+const onJoinRoom = require('./handlers/roomHandler')(io);
+const onNewMessage = require('./handlers/messageHandler')(io);
+const onDisconnect = require('./handlers/disconnectHandler')(io);
+
+const onConnection = (socket) => {
     console.info('user connected');
-    socket.on('JOIN__ROOM', async ({roomId, userName}) => {
-        const room = await Room.findOne({roomId: roomId}).exec();
-        if (room === null) {
-            socket.emit('SERVER:INVALID_ROOM');
-        } else {
-            const roomUser = await Room.findOne({'roomId': roomId, 'users.name': userName}).exec();
-            if (roomUser === null) {
-                socket.join(roomId);
-                const createdUser = await User.create({name: userName, socketId: socket.id});
-                room.users.push(createdUser);
-                await room.save();
-                socket.emit('SERVER:ALLOW_JOIN', {roomId, userName});
-                io.to(room.roomId).emit('SERVER:SET_USERS', room.users);
-            } else {
-                socket.emit('SERVER:USER_EXISTS');
-            }
-        }
-    });
-    socket.on('NEW_MESSAGE', async ({roomId, userName, text}) => {
-        const time = Date.now();
-        const message = {
-            from: userName,
-            time,
-            text
-        };
-        socket.broadcast.to(roomId).emit('SERVER:ADD_MESSAGE', message);
-    });
-    socket.on('disconnect', async () => {
-        console.log('bye bye :)');
-        const user = await User.findOne({socketId: socket.id}).exec();
-        if (user !== null) {
-            const room = await Room.findOne({'users.socketId': socket.id}).exec();
-            let index = room.users.indexOf(room.users.find((currUser) => currUser.name === user.name));
-            if (index > -1) {
-                room.users.splice(index, 1);
-            }
-            await room.save();
-            socket.leave(room.roomId);
-            await User.deleteOne({'socketId': socket.id}).exec();
-            io.to(room.roomId).emit('SERVER:SET_USERS', room.users);
-        }
-    });
-});
+    socket.on('JOIN__ROOM', onJoinRoom);
+    socket.on('NEW_MESSAGE', onNewMessage);
+    socket.on('disconnect', onDisconnect);
+}
+
+io.on('connection', onConnection);
 
 server.listen(config.get('port'), () => {
     console.info(`Server has been started on port: ${config.get('port')}`);
